c9f3a4f47ea58d8956fe85510ccff67b375e8cbc
Mark Pham <pham.manh.cuong.sg@gmail.com>
2023-05-22 23:10:08 +0700

    Push to merge to master

---
+++ eVoucherDatabaseWebService_DAL/Repositories/CampaignGameRepository.cs
@@ -0,0 +1,12 @@
+﻿using eVoucher_DAL.InfraStructure;
+
+namespace eVoucher_DAL.Repositories
+{
+    public interface ICampaignGameRepository : IRepository<CampaignGame>
+    { }
+
+    public class CampaignGameRepository : RepositoryBase<CampaignGame>, ICampaignGameRepository 
+    {
+        public CampaignGameRepository(eVoucherDbContext context) : base(context) { }
+    }
+}
